eef18f069d12965f7c5526a80fc89f40fd4aac1c
René van Schie <rene-v-schie@hotmail.com>
2016-05-11 14:35:07 +0200

    laatste versie php file

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
-<?php 
-$file = fopen("/home/pi/RaspberryPi/data.txt", "r") or die("Unable to open file!");
+<!doctype html>
+	<html lang="nl">
+	<charset="utf-8"/>
+	<head>
+		<link rel = "stylesheet"href="styles.css"/>
+		<title> </title>
+	</head>
 
-while(! feof($file))
-  {
-  echo fgets($file). "<br />";
-  }
+	<body>
+		<?php
+			require_once 'db_config.php';
+		?>
 
-fclose($file);
 
-?>
+		<?php
+			$sql = "SELECT data FROM data WHERE datatype = 'temperature' ORDER BY timestamp DESC LIMIT 1";
+			if(!$res = mysqli_query($link,$sql))
+			{
+				trigger_error('Fout in query: '.mysqli_error());
+			}
+			else
+			{
+				echo "Temperature: ";
+				while($row = mysqli_fetch_assoc($res))
+				{
+					echo $row['data'].'<br>';
+				}
+			}
+		?>
+		<br></br>
+		<img src="local.png"width="500"height="500"alt="QR code"class="qr"/>
+	</body>
+</html>
