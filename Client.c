f8d986b04a369e399d6fb5d269d80b688775d791
relas <rene-v-schie@hotmail.com>
2016-06-01 11:06:15 +0200

    client.c

--- Client.c
+++ Client.c
@@ -19,68 +19,83 @@ This code is compiled and run on the Macbook laptop as follows:
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <arpa/inet.h> 
 
-void error(char *msg) {
-    perror(msg);
-    exit(0);
-}
-
-void sendData( int sockfd, int x ) {
+void sendData( int sockfd, char* x ) {
   	int n;
-
-  	char buffer[32];
-  	sprintf( buffer, "%d\n", x );
-  	if ( (n = write( sockfd, buffer, strlen(buffer) ) ) < 0 )
+	printf("Send data\n");
+	printf("%s\n",x);
+  	if ( (n = send( sockfd, x, strlen(x),0 ) ) < 0 )
+	{
       		printf( "ERROR writing to socket");
-  	buffer[n] = '\0';
+	}
+	printf("Done sending\n");
 }
 
-int getData( int sockfd ) {
-  	char buffer[32];
-  	int n;
+char* getData( int sockfd ) {
+	printf("get Data\n");
+  	char buffer[2000];
 
-  	if ( (n = read(sockfd,buffer,31) ) < 0 )
+  	if ( (recv(sockfd,buffer,2000,0) ) < 0 )
+	{
        		printf( "ERROR reading from socket");
-  	buffer[n] = '\0';
-  	return atoi( buffer );
+	}
+	char* message = buffer;
+	printf("done receiving\n");
+  	return message;
 }
 
 int main(int argc, char *argv[])
 {
+	char deviceid[100];
     	int sockfd, portno = 8888, n;
     	char serverIp[] = "192.168.1.103";
     	struct sockaddr_in serv_addr;
     	struct hostent *server;
     	char buffer[256];
     	int data;
-
+	int device = 0;
+	char* deviceID = "test";
+	char* servermsg = "test";
     	if (argc < 3) {
       		// error( const_cast<char *>( "usage myClient2 hostname port\n" ) );
       		printf( "contacting %s on port %d\n", serverIp, portno );
       		// exit(0);
     	}
     	if ( ( sockfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 )
+	{
         	printf( "ERROR opening socket");
+	}
 
-    	if ( ( server = gethostbyname( serverIp ) ) == NULL ) 
+    	if ( ( server = gethostbyname( serverIp ) ) == NULL )
+	{
         	printf("ERROR, no such host\n");
-    
+	}
     	bzero( (char *) &serv_addr, sizeof(serv_addr));
     	serv_addr.sin_family = AF_INET;
     	bcopy( (char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     	serv_addr.sin_port = htons(portno);
-    	if ( connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+    	if ( connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+	{
         	printf( "ERROR connecting");
+	}
 
-    	for ( n = 0; n < 10; n++ ) {
-      		sendData( sockfd, n );
-      		data = getData( sockfd );
-      		printf("%d ->  %d\n",n, data );
-    	}
-    	sendData( sockfd, -2 );
-
+	sendData(sockfd,"ID/");
+	deviceID = getData(sockfd);
+	printf("%s\n",deviceID);
+	device = atoi(deviceID);
+	printf("%d\n",device);
+	while(deviceID == 0)
+	{}
+	sendData(sockfd,"IP192.168.134.246/test/");
+	servermsg = getData(sockfd);
+	printf("%s\n",servermsg);
+	while(servermsg == "test")
+	{}
+	char mes[500];
+	sprintf(mes,"DA%d/42/hi/",device);
+	printf("%s\n",mes);
+	sendData(sockfd,mes);
     	close( sockfd );
     	return 0;
 }
