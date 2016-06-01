2353d9bf786f827f31ed7e293096265e4bcab45b
rschie <rene.van.schie@alten.nl>
2016-06-01 13:21:27 +0200

    changes in test.c to 84600 to 86400

--- test.c
+++ test.c
@@ -5,7 +5,7 @@ int main(void){
 	struct timespec time;
 	clock_gettime(CLOCK_REALTIME,&time);
 	int year = time.tv_sec/31536000 + 1970;
-	int day = (time.tv_sec - (time.tv_sec/31536000-1970))/84600;
+	int day = (time.tv_sec - (time.tv_sec/31536000-1970))/86400;
 	printf("year: %d  day: %d \n",year);
 	return 0;
 }
